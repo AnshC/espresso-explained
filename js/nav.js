c8f171a7d1b03e679a2f06b78b2b0a752e662e0d
AnshC <63086888+AnshC@users.noreply.github.com>
2024-11-20 19:39:56 -0500

    test reference update

--- js/nav.js
+++ js/nav.js
@@ -1,7 +1,7 @@
 // Imports Navigation Component to "header" ID components on all HTML pages
 let header = document.getElementById("nav");
 
-fetch("../components/nav.html")
+fetch("components/nav.html")
     .then(response => {
         if (!response.ok) {
             throw new Error('Failed to load Navigation Component')
